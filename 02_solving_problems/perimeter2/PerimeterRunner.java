1389a7348babd9f4565b0b66bdcfdcfcc0925a53
sekR4 <sebastian.kraus.jena@gmail.com>
2020-12-14 09:17:21 +0100

    Week one = done :two_hearts:

--- 02_solving_problems/perimeter2/PerimeterRunner.java
+++ 02_solving_problems/perimeter2/PerimeterRunner.java
@@ -10,15 +10,41 @@ public class PerimeterRunner {
         // 3. for each current Point in Shape s:
         for (Point currPt : s.getPoints()) {
             // 1. Get distance between prev and current Point
-            double currDist = prevPt.distance(currPt)
+            double currDist = prevPt.distance(currPt);
             // 2. totalPerim += totalPerim + current distance
-            totalPerim = totalPerim + currDist
+            totalPerim = totalPerim + currDist;
             // Set prevPt = current point
-            prevPt = currPt
+            prevPt = currPt;
         }
-
         // 4. Return totalPerim
-        return totalPerim
+        return totalPerim;
+    }
+
+    public int getNumPoints(Shape s) {
+        int NumPoints = 0;
+        for (Point currPt : s.getPoints()) {
+            NumPoints = NumPoints + 1;
+        }
+        return NumPoints;
+    }
+
+
+
+    public double getAverageLength(Shape s) {
+        double len = getPerimeter(s);
+        int pts = getNumPoints(s);
+        return len/pts;
+    }
+
+    public double getLongest(Shape s) {
+        double lenmax = 0;
+        Point prevPt = s.getLastPoint();
+        for (Point currPt : s.getPoints()) {
+            double currDist = prevPt.distance(currPt);
+            if (currDist > lenmax) {
+                lenmax = currDist;
+            }
+        return lenmax;
     }
 
     public void testPerimeter() {
@@ -26,7 +52,14 @@ public class PerimeterRunner {
         FileResource fr = new FileResource();
         Shape s = new Shape(fr);
         double len = getPerimeter(s);
-        System.out.println("Umfang = " + len)
+        int pts = getNumPoints(s);
+        double avglen = getAverageLength(s);
+        double lenmax = getLongest(s);
+        System.out.println("Umfang = " + len);
+        System.out.println("NumPoints = " + pts);
+        System.out.println("avg(len) = " + avglen);
+        System.out.println("longest = " + lenmax);
+
     }
 
     public static void main(String[] args) {
