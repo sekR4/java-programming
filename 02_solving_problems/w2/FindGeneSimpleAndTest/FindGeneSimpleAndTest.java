f60d43305be01ac8b7a1e433c29bace70ec15739
sekR4 <sebastian.kraus.jena@gmail.com>
2020-12-15 11:33:13 +0100

    checking for valid gene strings

--- 02_solving_problems/w2/FindGeneSimpleAndTest/FindGeneSimpleAndTest.java
+++ 02_solving_problems/w2/FindGeneSimpleAndTest/FindGeneSimpleAndTest.java
@@ -4,12 +4,24 @@ public class FindGeneSimpleAndTest {
     public String findGeneSimple(String dna) {
         // start = "ATG"
         int startIdx = dna.indexOf("ATG");
-        // end = "TAA"
-        int endIdx = dna.indexOf("TAA", startIdx)+2;
 
+        // end = "TAA"
+        int endIdx = dna.indexOf("TAA", startIdx);
+
+        if ((startIdx == -1 || endIdx == -1) || (startIdx == -1 && endIdx == -1)) {
+            return "";
+        }
         // result = start + between + end
-        String result = dna.substring(startIdx,endIdx+1);
-        return result;
+        else {
+            // check if it is a valid Gene (ATG 3letters 3letters TAA)
+            if ((endIdx - startIdx) % 3 == 0) {
+                return dna.substring(startIdx,endIdx + 3);
+            }
+            else {
+                return "";
+            }
+
+        }
     }
 
     public void testFindGeneSimple() {
@@ -24,6 +36,22 @@ public class FindGeneSimpleAndTest {
         System.out.println("Found "+gene);
 
         dna = "XXXATGTAA000";
+        System.out.println("Checking "+dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Found "+gene);
+
+        // some dirty dna
+        dna = "XXXTAA000";
+        System.out.println("Checking "+dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Found "+gene);
+
+        dna = "XXX000";
+        System.out.println("Checking "+dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Found "+gene);
+
+        dna = "XXXATG000";
         System.out.println("Checking "+dna);
         gene = findGeneSimple(dna);
         System.out.println("Found "+gene);
