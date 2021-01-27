bc98010378fe7769c7f956d3843013ee51dd4d06
sekR4 <sebastian.kraus.jena@gmail.com>
2021-01-27 17:00:24 +0100

    started assignment

---
+++ 02_solving_problems/w2/StringsSecondAssignments/Part1.java
@@ -0,0 +1,63 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part1 {
+   public static int findStopCodon(String dna, int startIdx,String stopCodon){
+        int currentIdx = dna.indexOf(stopCodon, startIdx + 3);
+        while (currentIdx != -1) {
+            if ((currentIdx - startIdx) % 3 == 0) { return currentIdx; }
+            else { currentIdx = dna.indexOf(stopCodon, currentIdx + 1); }
+        }
+        return -1;
+    }
+    
+   public static String findGene(String dna, int startfrom) {
+       int startIdx = dna.indexOf("ATG", startfrom);
+       if (startIdx == -1) {return "";}
+       
+       int taAIdx = findStopCodon(dna,startIdx,"TAA");
+       int taGIdx = findStopCodon(dna,startIdx,"TAG");
+       int tGAIdx = findStopCodon(dna,startIdx,"TGA");
+       int minIdx = 0;
+
+       if (taAIdx == -1 || (tGAIdx != -1 && tGAIdx < taAIdx)) { minIdx = tGAIdx; }
+       else { minIdx = taAIdx;}
+       if (minIdx == -1 || (taGIdx != -1 && taGIdx < minIdx)) { minIdx = taGIdx; }
+       if (minIdx == -1) {return "";}
+
+       return dna.substring(startIdx,minIdx+3);
+    } 
+
+    public static void print_all_genes(String dna) {
+        int startIdx = 0;
+
+        while (true) {
+
+            String some_gene = findGene(dna, startIdx);
+            if (some_gene.isEmpty()) {break;}
+
+            System.out.println(some_gene);
+            startIdx = dna.indexOf(some_gene,startIdx) + some_gene.length();
+        }
+
+//        System.out.println("doing something");
+
+    }    
+   
+   public static void testFindStopCodon(){
+       String dna1 = "ATGATCGCTAATGCTTAAGCTATG";
+       String dna2 = "ATGATCGCTAATGCTTAAGCTATGATG............TAA";
+       System.out.println(findStopCodon(dna1,0,"TAA"));
+    }
+    
+   public static void testFindGene(){
+       String dna1 = "ATGATCGCTAATGCTTAAGCTATG";
+       String dna2 = "ATGATCGCTAATGCTTAAGCTATGATG............TAA";
+       //System.out.println(findGene(dna1,0));
+       print_all_genes(dna2);
+    }
+}
--- 02_solving_problems/w2/tmp/while-notes.java
+++ 02_solving_problems/w2/tmp/while-notes.java
@@ -26,7 +26,8 @@ public class WhileNotes {
                 currentIdx = dna.indexOf(stopCodon, currentIdx + 1);
             }
         }
-        return dna.length();
+//        System.out.println("Didn't find a Stop Codon of " + stopCodon);
+        return -1;//dna.length();
     }
 
     public static String findGeneNew(String dna) {
@@ -40,24 +41,81 @@ public class WhileNotes {
         int tGAIdx = findStopCodonIDX(dna,startIdx,"TGA");
 
         // Umstaendlicher Weg, das Minimum zwischen 3 Zahlen zu finden. Lang lebe Python :)
-        int tmp = Math.min(taAIdx,taGIdx);
-        int minIdx = Math.min(tmp,tGAIdx);
+//        int tmp = Math.min(taAIdx,taGIdx);
+//        int minIdx = Math.min(tmp,tGAIdx);
 
-        if (minIdx == dna.length()) {return "";}
+        int minIdx = 0;
+        // bool 1
+        if (taAIdx == -1 || (tGAIdx != -1 && tGAIdx < taAIdx)) {
+            minIdx = tGAIdx;
+        }
+        else {
+            minIdx = taAIdx;
+        }
 
+        // bool 2
+        if (minIdx == -1 || (taGIdx != -1 && taGIdx < minIdx)) {
+            minIdx = taGIdx;
+        }
+
+
+        if (minIdx == -1) {return "";}
+
+
+        return dna.substring(startIdx,minIdx+3);
+    }
+    public static String findGeneV3(String dna, int startfrom) {
+        int startIdx = dna.indexOf("ATG", startfrom);
+        if (startIdx == -1) {
+            return "";
+        }
+        int taAIdx = findStopCodonIDX(dna,startIdx,"TAA");
+        int taGIdx = findStopCodonIDX(dna,startIdx,"TAG");
+        int tGAIdx = findStopCodonIDX(dna,startIdx,"TGA");
+
+        int minIdx = 0;
+
+        if (taAIdx == -1 || (tGAIdx != -1 && tGAIdx < taAIdx)) { minIdx = tGAIdx; }
+        else { minIdx = taAIdx;}
+        if (minIdx == -1 || (taGIdx != -1 && taGIdx < minIdx)) { minIdx = taGIdx; }
+        if (minIdx == -1) {return "";}
 
         return dna.substring(startIdx,minIdx+3);
     }
 
 
+    public static void print_all_genes(String dna) {
+        int startIdx = 0;
+
+        while (true) {
+
+            String some_gene = findGeneV3(dna, startIdx);
+            if (some_gene.isEmpty()) {break;}
+
+            System.out.println(some_gene);
+            startIdx = dna.indexOf(some_gene,startIdx) + some_gene.length();
+        }
+
+//        System.out.println("doing something");
+
+    }
+
+    public static int howMany(String A, String B) {
+        //howMany(“GAA”, “ATGAACGAATTGAATC”) ==3
+        return 0;
+
+    }
     public static void main(String[] args){
-        String dna = "ATGATCGCTAATGCTTAAGCTATG";
+//        String dna = "ATGATCGCTAATGCTTAAGCTATG";
+        String dna = "ATGATCGCTAATGCTTAAGCTATGATG............TAA";
 //        dna = "XXXATG000000TAA000";
 //        dna = "XXX000000TAA000";
 //        dna = "XXX000000000";
 //        dna = "XXXATG000000TAATAA000";
 //        System.out.println(findGene(dna));
-        System.out.println(findStopCodonIDX(dna,0,"TAA"));
-        System.out.println(findGeneNew(dna));
+//        System.out.println(findStopCodonIDX(dna,0,"TAA"));
+//        System.out.println(findGeneNew(dna));
+//        if ("".isEmpty()) {System.out.println("This string is empty.");}
+        print_all_genes(dna);
     }
 }
