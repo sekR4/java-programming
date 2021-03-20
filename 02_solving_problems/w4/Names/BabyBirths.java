71089aefb15a652f8e7a3ca7cb3f2f07f76f6841
sekR4 <sebastian.kraus.jena@gmail.com>
2021-03-20 12:21:06 +0100

    saving last mods

--- 02_solving_problems/w4/Names/BabyBirths.java
+++ 02_solving_problems/w4/Names/BabyBirths.java
@@ -15,9 +15,23 @@ public class BabyBirths {
     }
     
     public void totalBirths(FileResource fr){
-        int total = 0;
-        for (CSVRecord row : fr.getCSVParser(false)) {total += Integer.parseInt(row.get(2));}    
-        System.out.println(total);
+        
+        int n_boys = 0;
+        int n_girls = 0;
+        
+        for (CSVRecord row : fr.getCSVParser(false)) {     
+            if (row.get(1).equals("M")) {n_boys += Integer.parseInt(row.get(2));}
+            else {n_girls += Integer.parseInt(row.get(2));}
+        }
+        
+        System.out.println("Total: "+ (n_boys+n_girls) +" Boys: "+n_boys+" Girls: "+n_girls);
+    }
+    
+    public void testTotalBirths(){
+        String path = "../us_babynames/us_babynames_by_year/yob2014.csv";
+        FileResource fr = new FileResource(path);
+        totalBirths(fr);
+    
     }
 
 }
